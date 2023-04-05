b27ebea6e6570d46a05a02ff194372717893c897
정종문 <whdans4005@naver.com>
2023-04-05 22:23:48 +0900

    0405 2222 jjm
    1. login scene
    2. signin scene
    3. 각 scene간 연결
    4. main 카메라 비율

--- Assets/Script/MainManage.cs
+++ Assets/Script/MainManage.cs
@@ -1,19 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class CameraMove : MonoBehaviour
+public class MainManage : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.UpArrow)) transform.position += Vector3.up / 50;
-        else if (Input.GetKey(KeyCode.DownArrow)) transform.position += Vector3.down / 50;
-    
+        
+    }
+    public void Click_Setting_Btn() {
+        SceneManager.LoadScene("LogInScene");
     }
 }
--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -2,18 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraMove : MonoBehaviour
+public class PlayerMove : MonoBehaviour
 {
+    public GameObject c;
     // Start is called before the first frame update
     void Start()
     {
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.UpArrow)) transform.position += Vector3.up / 50;
-        else if (Input.GetKey(KeyCode.DownArrow)) transform.position += Vector3.down / 50;
-    
+        Vector3 tempPos = c.transform.position;
+        tempPos -= new Vector3(0,0,-10);
+        transform.position = tempPos;
     }
 }
