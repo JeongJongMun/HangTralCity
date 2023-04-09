cd430935880e0ce2784071118aa6c740409e5728
정종문 <whdans4005@naver.com>
2023-04-10 00:41:21 +0900

    0410 0040
    1. 메인 맵 데모
    2. aws cognito 예제 코드 작성
    3. .net 버젼을 .net Framework로 설정함

--- Assets/Script/CameraMove.cs
+++ Assets/Script/CameraMove.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
+    public GameObject player;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,8 +13,13 @@ public class CameraMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.UpArrow)) transform.position += Vector3.up / 50;
-        else if (Input.GetKey(KeyCode.DownArrow)) transform.position += Vector3.down / 50;
+        //if (Input.GetKey(KeyCode.UpArrow)) transform.position += Vector3.up / 50;
+        //else if (Input.GetKey(KeyCode.LeftArrow)) transform.position += Vector3.left / 50;
+        //else if (Input.GetKey(KeyCode.RightArrow)) transform.position += Vector3.right / 50;
+        //else if (Input.GetKey(KeyCode.DownArrow)) transform.position += Vector3.down / 50;
+        Vector3 tempVec = player.transform.position;
+        tempVec.z = -10;
+        transform.position = tempVec;
     
     }
 }
---
+++ Assets/Script/PlayerMove.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKey( KeyCode.LeftArrow)) transform.Translate(-0.1f, 0f, 0f);
+        if (Input.GetKey(KeyCode.RightArrow)) transform.Translate(0.1f, 0f, 0f);
+        if (Input.GetKey(KeyCode.UpArrow)) transform.Translate(0f, 0.1f, 0f);
+        if (Input.GetKey(KeyCode.DownArrow)) transform.Translate(0f, -0.1f, 0f);
+
+    }
+}
