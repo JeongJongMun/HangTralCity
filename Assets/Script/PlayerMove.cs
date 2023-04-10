f91f295d942639cb19a45f58924950f218e981d4
simson0524 <simson0524@naver.com>
2023-04-10 21:39:10 +0900

    2023-04-10 2138
    강의동 데스크 collider 최종수정 완료 -> 맵 구현 최종완료

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    public float force = 0.05f;
     void Update()
     {
-        if (Input.GetKey( KeyCode.LeftArrow)) transform.Translate(-0.1f, 0f, 0f);
-        if (Input.GetKey(KeyCode.RightArrow)) transform.Translate(0.1f, 0f, 0f);
-        if (Input.GetKey(KeyCode.UpArrow)) transform.Translate(0f, 0.1f, 0f);
-        if (Input.GetKey(KeyCode.DownArrow)) transform.Translate(0f, -0.1f, 0f);
+        if (Input.GetKey( KeyCode.LeftArrow)) transform.Translate(-force, 0f, 0f);
+        if (Input.GetKey(KeyCode.RightArrow)) transform.Translate(force, 0f, 0f);
+        if (Input.GetKey(KeyCode.UpArrow)) transform.Translate(0f, force, 0f);
+        if (Input.GetKey(KeyCode.DownArrow)) transform.Translate(0f, -force, 0f);
 
     }
 }
