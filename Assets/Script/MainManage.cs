dc0e3e9d3e1a966273b4640ebbb50471407ad859
정종문 <whdans4005@naver.com>
2023-04-12 00:20:12 +0900

    0412 0220

--- Assets/Script/MainManage.cs
+++ Assets/Script/MainManage.cs
@@ -5,18 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class MainManage : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public GameObject menuBar;
+    private bool isMenuBtnClicked;
+    private void FixedUpdate()
     {
-        
+        if (isMenuBtnClicked)
+        {
+            Vector3 speed = Vector3.zero;
+            menuBar.transform.position = Vector3.SmoothDamp(menuBar.transform.position, new Vector3(540 + 190, 2340/2, 0), ref speed, 0.1f);
+        }
+        else if (isMenuBtnClicked == false)
+        {
+            Vector3 speed = Vector3.zero;
+            menuBar.transform.position = Vector3.SmoothDamp(menuBar.transform.position, new Vector3(1080 + 540 - 190, 2340 / 2, 0), ref speed, 0.1f);
+        }
+        if (Input.GetMouseButtonUp(0)) isMenuBtnClicked = false;
     }
     public void Click_Setting_Btn() {
         SceneManager.LoadScene("LogInScene");
+    }
+    public void Click_Menu_Btn()
+    {
+        isMenuBtnClicked = true;
     }
 }
