dfb6b1601781845856e82993399b21f77b36a904
정종문 <whdans4005@naver.com>
2023-04-17 15:35:45 +0900

    0417 1535 jjm

---
+++ Assets/Script/CameraOnPlayer.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine
+
+public class CameraOnPlayer : MonoBehaviour
+{
+    // 카메라가 플레이어 따라다니는 스크립트
+    // 강의동 등에서 사용
+    // 메인에서 사용 X
+    public GameObject player;
+
+    void Update()
+    {
+        Vector3 tempVec = player.transform.position;
+        tempVec.z = -10;
+        transform.position = tempVec;
+    
+    }
+}
