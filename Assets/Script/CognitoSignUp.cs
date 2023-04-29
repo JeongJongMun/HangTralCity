7c60d6e512ab986085bdeb376b518d60e56e3440
Jaehyeok Sim <116555114+simson0524@users.noreply.github.com>
2023-04-29 13:55:41 +0900

    2023-04-29 1355 sjh

--- Assets/Script/CognitoSignUp.cs
+++ Assets/Script/CognitoSignUp.cs
@@ -6,10 +6,55 @@ using System.Net;
 using Amazon.CognitoIdentityProvider;
 using Amazon.CognitoIdentity;
 using Amazon;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class CognitoSignUp : MonoBehaviour
 {
     private AmazonCognitoIdentityProviderClient cognitoService; // cognitoService 객체 선언
+
+    public InputField signUpEMail;
+    public InputField signUpPassWord;
+    public InputField signUpNickName;
+
+    public Button signUpSignUp;
+    public Button signUpBackToSignIn;
+
+    private string email;
+    private string password;
+    private string nickname;
+
+    private void Awake()
+    {
+        if (signUpNickName.GetComponent<InputField>().text == null)
+        {
+            nickname = " ";
+        }
+        else
+        {
+            nickname = signUpNickName.GetComponent<InputField>().text;
+        }
+
+        if (signUpEMail.GetComponent<InputField>().text == null)
+        {
+            email = " ";
+        }
+        else
+        {
+            email = signUpEMail.GetComponent<InputField>().text;
+
+        }
+
+        if (signUpPassWord.GetComponent<InputField>().text == null)
+        {
+            password = " ";
+        }
+        else
+        {
+            password = signUpPassWord.GetComponent<InputField>().text;
+
+        }
+    }
 
     void Start()
     {
@@ -18,7 +63,14 @@ public class CognitoSignUp : MonoBehaviour
 
         // Amazon Cognito 서비스의 객체를 인스턴스화
         cognitoService = new AmazonCognitoIdentityProviderClient(credentials, RegionEndpoint.APNortheast2);
-        SignUp(); // 유니티 실행하면 바로 회원가입 보내게끔 해놨음
+        
+        signUpSignUp.onClick.AddListener(SignUp); // 유니티 실행하면 바로 회원가입 보내게끔 해놨음
+        signUpBackToSignIn.onClick.AddListener(changeSignInScene);
+    }
+
+    void changeSignInScene()
+    {
+        SceneManager.LoadScene("SignInScene");
     }
 
     public async void SignUp()
@@ -27,14 +79,15 @@ public class CognitoSignUp : MonoBehaviour
         var signUpRequest = new SignUpRequest
         {
             ClientId = "1luokqrq9t4j8gag5kbnphunvu", // 클라이언트 ID (모든 사용자들 공통)
-            Username = "SimJaeHyeok", // 사용자 이름
-            Password = "12345678", // 비밀번호
+            Username = signUpNickName.ToString(), // 사용자 이름
+            Password = signUpPassWord.ToString(), // 비밀번호
             UserAttributes = new List<AttributeType> // aws에서 우리가 직접 설정한 필수 속성
             {
-                new AttributeType { Name = "email", Value = "simson0524@naver.com" },
-                new AttributeType { Name = "nickname", Value = "sjh" }
+                new AttributeType { Name = "email", Value = signUpEMail.ToString() },
+                new AttributeType { Name = "nickname", Value = signUpNickName.ToString() }
             }
         };
+
 
         // 예외 처리 및 성공 및 실패 처리
         try
