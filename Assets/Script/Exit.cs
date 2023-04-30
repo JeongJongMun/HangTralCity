53319e75c717163bee42c22c31be9dc865919332
정종문 <whdans4005@naver.com>
2023-04-30 14:01:55 +0900

    0430 1401 jjm
    1. 강의동 수정

--- Assets/Script/Exit.cs
+++ Assets/Script/Exit.cs
@@ -1,15 +1,30 @@
+using Amazon.EC2.Model;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Exit : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D collision)
+    public GameObject exit_btn;
+
+    private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.CompareTag("GangDoor"))
         {
-            SceneManager.LoadScene("MainScene");
+            exit_btn.SetActive(true);
         }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("GangDoor"))
+        {
+            exit_btn.SetActive(false);
+        }
+    }
+    public void ExitBtnClicked()
+    {
+        SceneManager.LoadScene("MainScene");
     }
 }
