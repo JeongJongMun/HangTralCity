ac86cae84745fded8ec227262002e6b037e564c8
simson0524 <simson0524@naver.com>
2023-05-05 14:55:00 +0900

    0505 1454 sjh

--- Assets/Script/MiniGameManage.cs
+++ Assets/Script/MiniGameManage.cs
@@ -9,10 +9,32 @@ public class MiniGameManage : MonoBehaviour
     public Button ranking;
     public Button main;
 
+    public Button setting;
+
+    public Button settingMain, settingSound;
+    public GameObject settingMAIN, settingSOUND;
+
+    bool settingButtonActive = false;
+
     void Start()
     {
         ranking.GetComponent<Button>().onClick.AddListener(EnterRanking);
         main.GetComponent<Button>().onClick.AddListener(EnterMain);
+
+        settingMain.GetComponent<Button>().onClick.AddListener(EnterMain);
+        settingSound.GetComponent<Button>().onClick.AddListener(EnterSoundSetting);
+
+        if (settingButtonActive == false)
+        {
+            setting.GetComponent<Button>().onClick.AddListener(SettingActiveTrue);
+            settingButtonActive = true;
+        }
+        else
+        {
+            setting.GetComponent<Button>().onClick.AddListener(SettingActiveTrue);
+            settingButtonActive = false;
+        }
+
     }
 
     public void EnterRanking()
@@ -23,5 +45,22 @@ public class MiniGameManage : MonoBehaviour
     public void EnterMain()
     {
         SceneManager.LoadScene("MainScene");
+    }
+
+    public void EnterSoundSetting()
+    {
+        SceneManager.LoadScene("SoundSettingScene");
+    }
+
+    public void SettingActiveTrue()
+    {
+        settingMAIN.SetActive(true);
+        settingSOUND.SetActive(true);
+    }
+
+    public void SettingActiveFalse()
+    {
+        settingMAIN.SetActive(false);
+        settingSOUND.SetActive(false);
     }
 }
