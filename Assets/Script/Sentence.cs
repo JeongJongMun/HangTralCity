4e195e58c6ac6e27fe2c366932c4d937bb061a2e
정종문 <whdans4005@naver.com>
2023-05-06 23:32:49 +0900

    0506 2329 JJM

--- Assets/Script/Sentence.cs
+++ Assets/Script/Sentence.cs
@@ -1,26 +1,27 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class Sentence : MonoBehaviour
 {
-    //public string[] sentences;
-    //public Transform chatTr;
-    //public GameObject chatBoxPrefab;
+    public string[] sentences;
+    public Transform chatTr;
+    public GameObject chatBoxPrefab;
 
-    //private void Start()
-    //{
+    private void Start()
+    {
         
-    //}
+    }
 
-    //public void Talk()
-    //{
-    //    GameObject go = Instantiate(chatBoxPrefab);
-    //    go.GetComponent<ChatSystem>().Ondialogue(sentences, chatTr);
-    //}
-    //private void OnMouseDown()
-    //{
-    //    Talk();
-    //}
+    public void Talk()
+    {
+        GameObject go = Instantiate(chatBoxPrefab);
+        go.GetComponent<ChatSystem>().Ondialogue(sentences, chatTr);
+    }
+    private void OnMouseDown()
+    {
+        Talk();
+    }
 }
+*/
